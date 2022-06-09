bfbf4c974745aa9e42523e2ffa18cedf99de0b20
Alex Celmer <alex.celmer@hotmail.com>
2022-06-08 21:00:53 -0300

    add auth middleware

--- app.js
+++ app.js
@@ -12,42 +12,52 @@ app.use(express.json());
 // Logic goes here
 // importing user context
 const School = require("./model/school");
+const school = require("./model/school");
 
 // Register
 app.post("/school/register", auth, async (req, res) => {
 	// our register logic goes here...
 	try {
 		// Get school input
-		const { name, email, users } = req.body;
-
+		const { name, email, inep, has_medium_teaching, accept_terms } = req.body;
+		if (!accept_terms) {
+			return res.status(400).send({ message: 'Você deve aceitar os termos de uso para continuar.' })
+		}
+		if (!has_medium_teaching) {
+			return res.status(400).send({ message: 'Atualmente somente ensino médio é suportado.' })
+		}
 		// Validate school input
-		if (!(email && name)) {
-			res.status(400).send({ message: 'Todos campos devem ser preenchidos!' });
+		if (!(email && name && inep)) {
+			return res.status(422).send({ message: 'Todos campos devem ser preenchidos!' });
 		}
 
 		// check if school already exist
 		// Validate if school exist in our database
-		const oldUser = await School.findOne({ email });
+		const oldSchool = await School.findOne({ inep });
 
-		if (oldUser) {
-			return res.status(409).send({ message: 'Usuário já registrado. Por favor faça login.' });
+		if (oldSchool) {
+			return res.status(422).send({ message: 'INEP já registrado. Por favor verifique se sua escola já não está cadastrada no sistema.' });
 		}
 
 		// Create school in our database
 		const school = await School.create({
 			name,
 			email: email.toLowerCase(), // sanitize: convert email to lowercase
-			users,
+			inep,
+			has_medium_teaching,
+			accept_terms
 		});
 
 		// return new school
-		return res.status(201).json(school);
+		// return res.status(201).json({message: 'Vai add'})
+		return res.status(201).json({ message: 'Escola adicionada com sucesso!' });
 	} catch (err) {
 		console.log(err);
 		return res.status(500).send({ message: 'Erro ao criar escola.' })
 	}
 });
 
+// updates school
 app.post("/school/:id", auth, async (req, res) => {
 	const id = req.params.id
 
@@ -58,9 +68,8 @@ app.post("/school/:id", auth, async (req, res) => {
 			name,
 			email: email.toLowerCase(), // sanitize: convert email to lowercase
 			users,
-			classes
 		});
-		return res.status(204).json(school)
+		return res.status(204).send({ school })
 	} catch (error) {
 		console.log(error);
 		return res.status(500).json({ message: "Erro ao editar escola" })
@@ -69,8 +78,11 @@ app.post("/school/:id", auth, async (req, res) => {
 
 });
 
-app.get("/schools/", auth, async (req, res) => {
+// get all schools
+app.get("/school/all", auth, async (req, res) => {
+	// return res.status(200)
 	const schools = await School.find()
+	console.log(schools);
 	if (schools) {
 		return res.status(200).json(schools)
 	} else {
@@ -78,6 +90,22 @@ app.get("/schools/", auth, async (req, res) => {
 	}
 });
 
+app.get("/school/my", auth, async (req, res) => {
+	try {
+		const schools = await school.find({ users: { "$in": req.user.user_id } })
+		if (schools){
+			return res.status(200).json(schools)
+		}
+		else{
+			return res.status(404).send({ message: "Nenhuma escola para seu usuário." })
+		}
+	} catch (error) {
+		console.log('error', error);
+		return res.status(500).send({ message: 'Erro ao procurar, entre em contato com o administrador.' })
+	}
+});
+
+// get school
 app.get("/school/:id", auth, async (req, res) => {
 	try {
 		const { id } = req.params
@@ -92,6 +120,7 @@ app.get("/school/:id", auth, async (req, res) => {
 	}
 })
 
+// delete school
 app.delete("/school/:id", auth, async (req, res) => {
 	try {
 		const { id } = req.params
--- model/school.js
+++ model/school.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
 const schoolSchema = new mongoose.Schema({
-	name: { type: String, default: null },
+	name: { type: String, default: null, unique: false },
 	email: { type: String, unique: true },
-	users: { type: Array },
-	classes: { type: Array },
+	inep: { type: Number, required: true },
+	has_medium_teaching: { type: Boolean },
+	accept_terms: { type: Boolean, required: true },
+	users: {type: Array, select: false}
+}, {
+	timestamps: true
 });
 
 module.exports = mongoose.model("school", schoolSchema);
